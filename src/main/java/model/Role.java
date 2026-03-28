40fde18c9f059a5b3224920f81d06b6b96d2ff59
Husain Habib <habib.h.e@outlook.com>
2026-03-29 01:42:37 +0300

    add role-based salary increment values to Role enum

--- src/main/java/model/Role.java
+++ src/main/java/model/Role.java
@@ -1,7 +1,15 @@
 package model;
 
+import lombok.Getter;
+
+@Getter
 public enum Role {
-    DIRECTOR,
-    MANAGER,
-    EMPLOYEE
+    DIRECTOR(0.05),
+    MANAGER(0.02),
+    EMPLOYEE(0.01);
+
+    private final double increment;
+    Role(double increment) {
+        this.increment = increment;
+    }
 }

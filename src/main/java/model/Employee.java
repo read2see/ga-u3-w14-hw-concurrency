cea53fd75dc1a2e7c8f315a23aed03c701d9b031
Husain Habib <habib.h.e@outlook.com>
2026-03-29 01:44:09 +0300

    feat/add Employee model with core attributes

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,19 @@
+package model;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Employee {
+
+    private String name;
+    private BigDecimal currentSalary;
+    private LocalDate joinDate;
+    private Role role;
+    private double projectCompletionRate;
+}

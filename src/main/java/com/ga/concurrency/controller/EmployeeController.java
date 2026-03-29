8d3b2e41568e1e34fad2b8ef6ac7a7f62d9329da
Husain Habib <habib.h.e@outlook.com>
2026-03-29 09:02:50 +0300

    feat/add EmployeeController

--- src/main/java/com/ga/concurrency/controller/EmployeeController.java
+++ src/main/java/com/ga/concurrency/controller/EmployeeController.java
@@ -1,4 +1,32 @@
 package com.ga.concurrency.controller;
 
+import com.ga.concurrency.model.Employee;
+import com.ga.concurrency.service.EmployeeService;
+import com.opencsv.exceptions.CsvValidationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "/")
 public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public String index(Model model) throws IOException, CsvValidationException {
+        List<Employee> employees = employeeService.processEmployees();
+        model.addAttribute("employees", employees);
+        return "index";
+    }
 }
